c9d49c59b9cd3de55268d771dd466f132119ccd0
Anas <97516429+Anas-AI10@users.noreply.github.com>
2022-09-11 16:27:26 +0530

    Add files via upload

---
+++ 3int.cpp
@@ -0,0 +1,47 @@
+#include<iostream.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+ int a;
+ int b;
+ int c;
+ cout<<"\n Enter value:'<<a;
+ cin>>a;
+ cout<<"\n Enter value:'<<b;
+ cin>>b;
+ cout<<"\n Enter value:'<<c;
+ cin>>c;
+ if(a>b)
+ {
+  cout<<"a is greater";
+ }
+ else if
+ {
+ cout<<"b is greater";
+ }
+ if(a>c)
+ {
+  cout<<"a is greater";
+ }
+ else if
+ {
+ cout<<"c is greater";
+ }
+ if(b>c)
+ {
+  cout<<"b is greater";
+ }
+ else if
+ {
+ cout<<"c is greater";
+ }
+ if(c>a)
+ {
+  cout<<"c is greater";
+ }
+ else if
+ {
+ cout<<"a is greater";
+ }
+ 
---
+++ ctof.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+ int f;
+ cout<<"enter temperature in fahrenheit:";
+ cin>>f;
+ int c=32-32*5/9=0;
+ cout<<"celsius is :"<<c;
+ getch
+}
+ 
